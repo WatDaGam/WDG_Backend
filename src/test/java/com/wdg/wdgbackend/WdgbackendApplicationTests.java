a29e6361f0bf74c031dba0c6ab6dca75d0f13478
yback1223 <yoyob1223@gmail.com>
2023-10-30 20:07:30 +0900

    Add development environment setting

---
+++ src/test/java/com/wdg/wdgbackend/WdgbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdg.wdgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdgbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
