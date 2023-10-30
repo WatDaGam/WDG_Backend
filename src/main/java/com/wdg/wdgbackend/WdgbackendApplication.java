883079e82301e18316eac4f82995f382f2c635a9
yback1223 <yoyob1223@gmail.com>
2023-10-30 20:07:30 +0900

    Add development environment setting

---
+++ src/main/java/com/wdg/wdgbackend/WdgbackendApplication.java
@@ -0,0 +1,13 @@
+package com.wdg.wdgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdgbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdgbackendApplication.class, args);
+	}
+
+}
