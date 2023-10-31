6ae38f9aa2b7b3462aed5234b34f89e7bd8e4ab2
yback1223 <yoyob1223@gmail.com>
2023-10-31 18:55:54 +0900

    WBS-50 Add getout controller which means quit

---
+++ src/main/java/com/wdg/wdgbackend/controller/GetoutController.java
@@ -0,0 +1,29 @@
+package com.wdg.wdgbackend.controller;
+
+import com.wdg.wdgbackend.controller.service.GetoutService;
+import org.apache.ibatis.annotations.Delete;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/getout")
+public class GetoutController {
+
+	private final GetoutService getoutService;
+
+	@Autowired
+	public GetoutController(GetoutService getoutService) {
+		this.getoutService = getoutService;
+	}
+
+	@DeleteMapping
+	public ResponseEntity<String> getout(@RequestParam("nickname") String nickname) {
+		getoutService.deleteUser(nickname);
+		return new ResponseEntity<>("Deleted", HttpStatus.OK);
+	}
+}
