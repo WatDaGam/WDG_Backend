0b22cac0f81d2113d7edcfbc9dd0b0ff732df3c1
yback1223 <yoyob1223@gmail.com>
2023-11-03 12:22:41 +0900

    Update server - header upper case, class name change

--- src/main/java/com/wdg/wdgbackend/controller/WithdrawalController.java
+++ src/main/java/com/wdg/wdgbackend/controller/WithdrawalController.java
@@ -1,24 +1,22 @@
 package com.wdg.wdgbackend.controller;
 
-import com.wdg.wdgbackend.controller.service.GetoutService;
-import org.apache.ibatis.annotations.Delete;
+import com.wdg.wdgbackend.controller.service.WithdrawalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/withdrawl")
-public class GetoutController {
+@RequestMapping("/withdrawal")
+public class WithdrawalController {
 
-	private final GetoutService getoutService;
+	private final WithdrawalService getoutService;
 
 	@Autowired
-	public GetoutController(GetoutService getoutService) {
+	public WithdrawalController(WithdrawalService getoutService) {
 		this.getoutService = getoutService;
 	}
 
