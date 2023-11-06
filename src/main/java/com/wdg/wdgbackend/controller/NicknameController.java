ac948f9b86f37e8d5009b72c2ad1b5ab73899265
yback1223 <yoyob1223@gmail.com>
2023-11-06 22:05:31 +0900

    Fix to return new tokens when user sets nickname

--- src/main/java/com/wdg/wdgbackend/controller/NicknameController.java
+++ src/main/java/com/wdg/wdgbackend/controller/NicknameController.java
@@ -1,8 +1,10 @@
 package com.wdg.wdgbackend.controller;
 
+import com.wdg.wdgbackend.controller.service.LoginService;
 import com.wdg.wdgbackend.controller.service.TokenService;
 import com.wdg.wdgbackend.controller.service.NicknameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.*;
 public class NicknameController {
 
 	private final NicknameService nicknameService;
+	private final LoginService loginService;
 	private final TokenService tokenService;
 
 	@Autowired
-	public NicknameController(NicknameService nicknameService, TokenService tokenService) {
+	public NicknameController(NicknameService nicknameService, LoginService loginService, TokenService tokenService) {
 		this.nicknameService = nicknameService;
+		this.loginService = loginService;
 		this.tokenService = tokenService;
 	}
 
@@ -29,8 +33,11 @@ public class NicknameController {
 	@PostMapping("/set")
 	public ResponseEntity<String> setNickname(@RequestBody String nickname, @RequestHeader("Authorization") String authorizationHeader) {
 		Long userId = tokenService.getIdFromAccessToken(authorizationHeader);
+		Long snsId = tokenService.getSnsIdFromAccessToken(authorizationHeader);
+
 		if (nicknameService.isNicknameDuplicated(nickname)) return new ResponseEntity<>("duplicated", HttpStatus.OK);
 		nicknameService.setNickname(userId, nickname);
-		return new ResponseEntity<>("ok", HttpStatus.OK);
+		HttpHeaders responseHeaders = loginService.getHttpHeaders(snsId);
+		return new ResponseEntity<>(responseHeaders, HttpStatus.OK);
 	}
 }
