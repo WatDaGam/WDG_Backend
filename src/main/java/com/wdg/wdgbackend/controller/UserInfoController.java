204db5e256205aca900384f7c5b56959ff8055b9
yback1223 <yoyob1223@gmail.com>
2023-11-06 18:21:06 +0900

    Refactor controller not to use tokenservice

--- src/main/java/com/wdg/wdgbackend/controller/UserInfoController.java
+++ src/main/java/com/wdg/wdgbackend/controller/UserInfoController.java
@@ -5,6 +5,7 @@ import com.wdg.wdgbackend.controller.service.UserInfoService;
 import com.wdg.wdgbackend.model.entity.User;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,19 +14,15 @@ import org.springframework.web.bind.annotation.*;
 public class UserInfoController {
 
 	private final UserInfoService userInfoService;
-	private final TokenService tokenServcie;
 
 	@Autowired
-	public UserInfoController(UserInfoService userInfoService, TokenService tokenServcie) {
+	public UserInfoController(UserInfoService userInfoService) {
 		this.userInfoService = userInfoService;
-		this.tokenServcie = tokenServcie;
 	}
 
 	@GetMapping
-	public ResponseEntity<?> getUserInfo(@RequestHeader("Authorization") String authorizationHeader) {
-		Long idFromAccessToken = tokenServcie.getIdFromAccessToken(authorizationHeader);
-		User user = userInfoService.getUser(idFromAccessToken);
-
-		return ResponseEntity.ok(userInfoService.makeUserJSONObject(user).toString());
+	public ResponseEntity<String> getUserInfo(@RequestHeader("Authorization") String authorizationHeader) {
+		String userInfoJSON = userInfoService.makeUserJSONObject(authorizationHeader);
+		return new ResponseEntity<>(userInfoJSON, HttpStatus.OK);
 	}
 }
