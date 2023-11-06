fe824942457e7da982e0cbc2ff448ca3c23e0f9f
yback1223 <yoyob1223@gmail.com>
2023-11-06 19:57:39 +0900

    WBS-86 WBS-87 Add likeNum +1 and -1 API including handling Concurrency issue

--- src/main/java/com/wdg/wdgbackend/model/mapper/StoryMapper.java
+++ src/main/java/com/wdg/wdgbackend/model/mapper/StoryMapper.java
@@ -10,6 +10,15 @@ public interface StoryMapper {
 	@Options(useGeneratedKeys = true, keyProperty = "id")
 	void insert(Story story);
 
+	@Update("UPDATE story SET like_num = like_num + 1 WHERE id = #{storyId}")
+	void likePlus(Long storyId);
+
+	@Update("UPDATE story SET like_num = like_num - 1 WHERE id = #{storyId}")
+	void likeMinus(Long storyId);
+
+	@Select("SELECT like_num FROM story WHERE id = #{storyId} FOR UPDATE")
+	void lockStory(Long storyId);
+
 	@Select("SELECT * from story WHERE id = #{storyId}")
 	Story getStory(Long storyId);
 
